17060bac2bef113670763f0f89b68c87fb133a34
Agila A <a.agila2005@gmail.com>
2026-04-06 19:31:26 +0530

    Added changes in order to host

--- forMe-backend/server.js
+++ forMe-backend/server.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log(err));
 
 // start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
